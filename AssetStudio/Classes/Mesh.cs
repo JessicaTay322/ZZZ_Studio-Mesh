f316d6fa2c795f10b0b35f9d8510de4acd57c568
Jessica <33334474+JessicaTay322@users.noreply.github.com>
2024-08-01 21:39:51 +1000

    Update Mesh.cs
    Guessing, I think I have to be close now

--- AssetStudio/Classes/Mesh.cs
+++ AssetStudio/Classes/Mesh.cs
@@ -738,8 +738,8 @@ namespace AssetStudio
                 var m_CompressLevelVertexData = reader.ReadInt32();
                 //var m_CompressLevelNormalAndTangent = reader.ReadInt32();
                 var m_Pusher1 = reader.ReadBoolean();
-                var m_Pusher2 = reader.ReadBoolean();
-                var m_Pusher3 = reader.ReadBoolean();
+                //var m_Pusher2 = reader.ReadBoolean();
+                //var m_Pusher3 = reader.ReadBoolean();
                 //var m_CompressLevelTexCoordinates = reader.ReadInt32();
                 //var m_CompressLevelTriangles = reader.ReadInt32();
             }
